7f9e8c5bbaecc6b5b9b283653e157e3af988ad4e
KimWang <77255052@qq.com>
2019-01-22 16:15:05 +0800

    更新AES算法;

--- assets/dist/js/tool/aes.js
+++ assets/dist/js/tool/aes.js
@@ -89,6 +89,26 @@ var S_BOX = [
 	[0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16]
 ]
 
+R_S_BOX = [
+  [0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb],
+  [0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb],
+  [0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e],
+  [0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25],
+  [0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92],
+  [0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84],
+  [0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06],
+  [0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b],
+  [0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73],
+  [0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e],
+  [0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b],
+  [0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4],
+  [0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f],
+  [0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef],
+  [0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61],
+  [0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d] 
+];
+
+
 var XOR_ARR_2 = function(a, b)
 {
 	var ret = [];
@@ -202,6 +222,17 @@ var SubBytes = function(block){
 	}
 }
 
+var InvSubBytes = function(block){
+	var len = block.length;
+	for(var i = 0; i < len; i++){
+		for(var j = 0; j < len; j++){
+			var x = (block[i][j] & 0xf0) >>> 4;
+			var y = (block[i][j] & 0x0f);
+			block[i][j] = R_S_BOX[x][y];
+		}
+	}
+}
+
 var ShiftRows = function(block)
 {
 	var len = block.length;
@@ -218,6 +249,22 @@ var ShiftRows = function(block)
 	}
 }
 
+var InvShiftRows = function(block)
+{
+	var len = block.length;
+	for(var j = 0; j < len; j++){
+		var count = j;
+		while(count > 0){
+			var tmp = block[len - 1][j];
+			for(var i = len - 1; i > 0; i--){
+				block[i][j] = block[i - 1][j]
+			}
+			block[0][j] = tmp;
+			count = count - 1;
+		}
+	}
+}
+
 var MixColumns = function(block)
 {
 	for(var i = 0; i < block.length; i++){
@@ -225,6 +272,21 @@ var MixColumns = function(block)
 		var v1 = GMul(0x01, block[i][0]) ^ GMul(0x02, block[i][1]) ^ GMul(0x03, block[i][2]) ^ GMul(0x01, block[i][3]);
 		var v2 = GMul(0x01, block[i][0]) ^ GMul(0x01, block[i][1]) ^ GMul(0x02, block[i][2]) ^ GMul(0x03, block[i][3]);
 		var v3 = GMul(0x03, block[i][0]) ^ GMul(0x01, block[i][1]) ^ GMul(0x01, block[i][2]) ^ GMul(0x02, block[i][3]);
+
+		block[i][0] = v0 & 0xff;
+		block[i][1] = v1 & 0xff;
+		block[i][2] = v2 & 0xff;
+		block[i][3] = v3 & 0xff;
+	}
+}
+
+var InvMixColumns = function(block)
+{
+	for(var i = 0; i < block.length; i++){
+		var v0 = GMul(0x0e, block[i][0]) ^ GMul(0x0b, block[i][1]) ^ GMul(0x0d, block[i][2]) ^ GMul(0x09, block[i][3]);
+		var v1 = GMul(0x09, block[i][0]) ^ GMul(0x0e, block[i][1]) ^ GMul(0x0b, block[i][2]) ^ GMul(0x0d, block[i][3]);
+		var v2 = GMul(0x0d, block[i][0]) ^ GMul(0x09, block[i][1]) ^ GMul(0x0e, block[i][2]) ^ GMul(0x0b, block[i][3]);
+		var v3 = GMul(0x0b, block[i][0]) ^ GMul(0x0d, block[i][1]) ^ GMul(0x09, block[i][2]) ^ GMul(0x0e, block[i][3]);
 
 		block[i][0] = v0 & 0xff;
 		block[i][1] = v1 & 0xff;
@@ -250,6 +312,19 @@ var blockEncrypt = function(block, keys, round)
 		ShiftRows(block);
 		if(i < (round - 1)){ MixColumns(block); }
 		pIdx += 4;
+		AddRoundKey(block, keys, pIdx);
+	}
+}
+
+var blockDecrypt = function(block, keys, round)
+{
+	var pIdx = 4 * round;
+	AddRoundKey(block, keys, pIdx);
+	for(var i = 0; i < round; i++){
+		InvShiftRows(block);
+		InvSubBytes(block);
+		if(i < (round - 1)){ InvMixColumns(block); }
+		pIdx -= 4;
 		AddRoundKey(block, keys, pIdx);
 	}
 }
@@ -398,21 +473,63 @@ aes_encrypt = function(plainText, cipherKeysText, keyLength, mode, padding, ivTe
 	return output;
 }
 
-
-var output = aes_encrypt("在线AES加密 | AES解密 - 在线工具", "0123456789abcdef", 192, "ECB", "pkcs5padding", "");
-
-dump = function(output)
+//keyLength 键长度 ASEKEY128, ASEKEY192, ASEKEY256
+//mode    分组模式,分别有ECB, CBC, CFB, OFB, CTR
+//padding 填充模式,分别有zeropadding, pkcs5padding(默认), pkcs7padding, ISO10126, ansix923, ISO/IEC7816-4
+//iv      偏移量
+aes_decrypt = function(data, cipherKeysText, keyLength, mode, padding, ivText)
 {
-	var arr = [];
-	for(var i = 0; i < output.length; i++){
-		var item = output[i].toString(16);
-		if(item.length == 1){ item = "0" + item;}
-		arr.push(item);
+	//if(plainText == null || plainText.length == 0){return "";}
+
+	if(keyLength == null){keyLength = ASEKEY128;}
+	if(mode == null){mode = "ECB";}
+	if(padding == null){padding = "pkcs5padding";}
+
+	var round = 10;
+	switch(keyLength){
+		case ASEKEY128: round = 10; break;
+		case ASEKEY192: round = 12; break;
+		case ASEKEY256: round = 14; break;
 	}
-	return arr.join("");
+
+
+	// var data = stringToByte(plainText);
+	// aes_add_padding(data, mode, padding);
+
+	var cipherKeys = convertCipherKey(cipherKeysText, keyLength);
+	var keys = keyExpansion(cipherKeys, round);
+	var iv = convertIV(ivText);
+
+	var output = [];
+	for(var i = 0; i < data.length; i += 16){
+		var arr = data.slice(i, i + 16);
+		var block = convertBlock(arr);
+		console.log("block", block);
+		blockDecrypt(block, keys, round);
+		copyToArray(output, block);
+	}
+
+	console.log("aes_decrypt", data);
+
+	return output;
 }
 
-console.log(dump(output));
+
+
+// var output = aes_encrypt("在线AES加密 | AES解密 - 在线工具", "0123456789abcdef", 192, "ECB", "pkcs5padding", "");
+
+// dump = function(output)
+// {
+// 	var arr = [];
+// 	for(var i = 0; i < output.length; i++){
+// 		var item = output[i].toString(16);
+// 		if(item.length == 1){ item = "0" + item;}
+// 		arr.push(item);
+// 	}
+// 	return arr.join("");
+// }
+
+// console.log(dump(output));
 
 
 
